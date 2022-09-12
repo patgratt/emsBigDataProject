8b4a40f38354cbbf970365f3f630e94176f0d542
33patburke <33patburke@gmail.com>
2022-09-12 16:42:49 -0400

    had to remove dot command

--- stage2/sqlImportScript.py
+++ stage2/sqlImportScript.py
@@ -38,31 +38,30 @@ with open(filepath, "r") as csvfile:
     contents = csv.reader(csvfile)
     next(contents)
 
-# Define SQL statement to insert data into the computedElements table
-insert = '''
-INSERT INTO computedElements (PcrKey,
-    USCensusRegion,
-    USCensusDivision,
-    NasemsoRegion,
-    Urbanicity,
-    ageinyear,
-    EMSDispatchCenterTimeSec,
-    EMSChuteTimeMin,
-    EMSSystemResponseTimeMin,
-    EMSSceneResponseTimeMin,
-    EMSSceneTimeMin,
-    EMSSceneToPatientTimeMin,
-    EMSTransportTimeMin,
-    EMSTotalCallTimeMin)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-'''
+    # Define SQL statement to insert data into the computedElements table
+    insert = '''
+    INSERT INTO computedElements (PcrKey,
+        USCensusRegion,
+        USCensusDivision,
+        NasemsoRegion,
+        Urbanicity,
+        ageinyear,
+        EMSDispatchCenterTimeSec,
+        EMSChuteTimeMin,
+        EMSSystemResponseTimeMin,
+        EMSSceneResponseTimeMin,
+        EMSSceneTimeMin,
+        EMSSceneToPatientTimeMin,
+        EMSTransportTimeMin,
+        EMSTotalCallTimeMin)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    '''
 
-# Execute insert statement
-cursor.executemany(insert, contents)
+    # Execute insert statement
+    cursor.executemany(insert, contents)
 
 # SQL query to retrieve first 50 rows from the computedElements table to verify that the
 # data from the csv file has been successfully inserted into the table
-cursor.execute('.mode table')
 select100 = 'SELECT * FROM ComputedElements LIMIT 50;'
 rows = cursor.execute(select100).fetchall()
 
