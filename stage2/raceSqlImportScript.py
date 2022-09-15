6a954dc5a8092feda523c83e819211e854d64f7d
33patburke <33patburke@gmail.com>
2022-09-14 23:47:45 -0400

    One little fix, SQL does not like that I used slashes in the column names, also edited SQL import script

--- stage2/raceSqlImportScript.py
+++ stage2/raceSqlImportScript.py
@@ -18,13 +18,17 @@ print("Creating table now...")
 cursor = connection.cursor()
 
 # Define SQL statement to create table
+# Use integer for now even though some of these are really booleans
 create_table = '''
-CREATE TABLE IF NOT EXISTS pcrpatientracegroup (
-    PcrPatientRaceGroupKey INT PRIMARY KEY NOT NULL,
-    PcrKey INT NOT NULL,
-    ePatient_14 INT,
-    FOREIGN KEY(PcrKey) REFERENCES computedElements(PcrKey)
-);
+CREATE TABLE IF NOT EXISTS raceTable (
+    PcrKey INT PRIMARY KEY NOT NULL,
+    black INTEGER,  
+    white INTEGER,  
+    hispanic/latino INTEGER,  
+    asian INTEGER,  
+    americanIndian/alaskaNative INTEGER,  
+    nativeHawaiian/otherPacificIslander INTEGER
+    );
 '''
 
 # Execute create table statement
@@ -32,7 +36,7 @@ cursor.execute(create_table)
 print("Table successfully created!")
 print(f"Time to create table = {format_timespan(time.time() - t_create_table)}")
 
-inputpath = ('/Users/patrickburke/Library/CloudStorage/OneDrive-EmoryUniversity/ECON496RW/cleaned_small/pcrpatientracegroup.csv')
+inputpath = ('/Users/patrickburke/Library/CloudStorage/OneDrive-EmoryUniversity/ECON496RW/raceInProgress/raceV3.csv')
 
 t_start_read = time.time()
 print("Reading csv file...")
@@ -43,10 +47,14 @@ with open(inputpath, "r") as csvfile:
 
         # Define SQL statement to insert data into the computedElements table
         insert = '''
-        INSERT INTO pcrpatientracegroup (PcrPatientRaceGroupKey,
-            PcrKey,
-            ePatient_14)
-        VALUES (?, ?, ?);
+        INSERT INTO raceTable (PcrKey,
+            black,
+            white,
+            hispanic/latino,
+            asian,
+            americanIndian/alaskaNative,
+            nativeHawaiian/otherPacificIslander)
+        VALUES (?, ?, ?, ?, ?, ?, ?);
         '''
 
         # Execute insert statement
